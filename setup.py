216d20502788474d9e3a13aaa751190deee8a4ab
Dhruv <dhruvparmar70441@gmail.com>
2025-12-24 00:50:10 +0530

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    
+    HYPHEN_E_DOT = "-e ."
+    with open(file_path, encoding='utf-16') as f:
+        requirements=[
+            line.strip()
+            for line in f.read().splitlines()
+            if line.strip() and line.strip() != HYPHEN_E_DOT
+        ]
+    return requirements
+
+
+setup(
+    name="loan-eligibility",
+    version="0.1.0",
+    description="Loan eligibility prediction system",
+    author="Dhruv Parmar",
+    email="dhruvparmar70441@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(file_path='requirements.txt')
+)
